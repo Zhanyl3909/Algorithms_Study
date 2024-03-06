1dac6deef1dd61c5b8b04e3e103fc1bc71869294
Zhanyl3909 <dzh.2002z@gmail.com>
2024-03-06 22:18:01 +0900

    add dao

---
+++ SpringBoot/src/11-data-access-object/src/main/java/com/zhanylai11/cruddemo/CruddemoApplication.java
@@ -0,0 +1,39 @@
+package com.zhanylai11.cruddemo;
+
+import com.zhanylai11.cruddemo.dao.StudentDAO;
+import com.zhanylai11.cruddemo.entity.Student;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+		return runner -> {
+			createStudent(studentDAO);
+		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		//create the student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Zhanylai", "Dzholchieva", "dzh.2002z@gmail.com");
+
+		//save
+		System.out.println("Saving the student");
+		studentDAO.save(tempStudent);
+
+		//display id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
+
+	}
+
+}
