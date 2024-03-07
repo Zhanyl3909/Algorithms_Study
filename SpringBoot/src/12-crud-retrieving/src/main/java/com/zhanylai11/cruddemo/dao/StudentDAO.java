53a9abc8ef3ebf759e66b4d21aa31c5007fd24e4
Zhanyl3909 <dzh.2002z@gmail.com>
2024-03-07 13:23:53 +0900

    add crud reading

---
+++ SpringBoot/src/12-crud-retrieving/src/main/java/com/zhanylai11/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,9 @@
+package com.zhanylai11.cruddemo.dao;
+
+import com.zhanylai11.cruddemo.entity.Student;
+
+public interface StudentDAO {
+     void save(Student theStudent);
+
+     Student findById(Integer id);
+}
